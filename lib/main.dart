53bbb7741600538bb5e5de21f4c9374aa704f944
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-12 06:53:04 +0530

    added AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,29 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text("Flutter"),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: appBar(),
+      ),
+    );
   }
 }
